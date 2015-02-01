2efe39d6fb3ce18a3177960ebffcb4ae807aa672
Ben Faul <ben.fau@gmail.com>
2015-02-01 15:01:27 -0800

    Update web site with architecure documents

--- www/deploy.sh
+++ www/deploy.sh
@@ -5,3 +5,4 @@ scp -i ~ben/certs/rtb4free_key.pem contact.html ubuntu@rtb4free.com:/usr/share/n
 scp -i ~ben/certs/rtb4free_key.pem details.html ubuntu@rtb4free.com:/usr/share/nginx/www/details.html
 scp -i ~ben/certs/rtb4free_key.pem license.html ubuntu@rtb4free.com:/usr/share/nginx/www/license.html
 scp -i ~ben/certs/rtb4free_key.pem overview.html ubuntu@rtb4free.com:/usr/share/nginx/www/overview.html
+scp -i ~ben/certs/rtb4free_key.pem arch/* ubuntu@rtb4free.com:/usr/share/nginx/www/arch
