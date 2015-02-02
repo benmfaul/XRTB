0824db943ffe2b69cfbfbf0d914a7defc28dd891
Ben Faul <ben.faul@gmail.com>
2015-02-02 10:48:42 -0800

    Add javadoc to deploy

--- www/deploy.sh
+++ www/deploy.sh
@@ -7,3 +7,4 @@ scp -i ~ben/certs/rtb4free_key.pem license.html ubuntu@rtb4free.com:/usr/share/n
 scp -i ~ben/certs/rtb4free_key.pem overview.html ubuntu@rtb4free.com:/usr/share/nginx/www/overview.html
 scp -i ~ben/certs/rtb4free_key.pem arch/* ubuntu@rtb4free.com:/usr/share/nginx/www/arch
 scp -i ~ben/certs/rtb4free_key.pem -r ../html ubuntu@rtb4free.com:/usr/share/nginx/www/html
+scp -i ~ben/certs/rtb4free_key.pem -r ../javadoc ubuntu@rtb4free.com:/usr/share/nginx/www/javadoc
