da4dae300543f44d6251037bb09e0f0b43964349
Ben Faul <ben.faul@gmail.com>
2015-03-02 10:45:47 -0800

    Implement the dynamic attributes

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,5 @@
 import java.io.IOException;
 
-import org.codehaus.jackson.JsonProcessingException;
-
-import com.xrtb.exchanges.Nexage;
-import com.xrtb.pojo.BidRequest;
-
-
 /**
  * String indexOf is faster than StringBuffer
  * Replace, StringBuffer much faster, at leat 10x
@@ -14,10 +8,22 @@ import com.xrtb.pojo.BidRequest;
  */
 public class Test {
 
-	public static void main(String []args) throws JsonProcessingException, IOException {
+	public static void main(String []args) throws Exception {
 
-		String s = "%7Bad_id%7D";
-		System.out.println(s.matches(".*Bad_id.*"));
+		StringBuilder a = new StringBuilder("                                   2210-91-0293-0193-012");
+		long start = System.currentTimeMillis();
+		for (int i=0;i<1000000;i++) {
+			a.replace(0, a.length(), "1039-0192-0492-3049-0239-23094-02394-02");
+		}
+		long stop = System.currentTimeMillis() - start;
 		
+		StringBuffer b = new StringBuffer("                                   2210-91-0293-0193-012");
+		start = System.currentTimeMillis();
+		for (int i=0;i<1000000;i++) {
+			b.replace(0, b.length(), "1039-0192-0492-3049-0239-23094-02394-02");
+		}
+		long stop1 = System.currentTimeMillis() - start;
+		
+		System.out.println("A="+stop+", B="+stop1);
 	}
 }
