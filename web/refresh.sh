0d6a1c533df7000c728375a3d71963a12c18999f
Ben Faul <ben.faul@gmail.com>
2015-11-20 16:10:13 -0800

    Add admin to the deployment

--- web/refresh.sh
+++ web/refresh.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 scp -i ~ben/certs/rtb4free_key.pem exchange.html ubuntu@rtb4free.com:XRTB/web
 scp -i ~ben/certs/rtb4free_key.pem login.html ubuntu@rtb4free.com:XRTB/web
+scp -i ~ben/certs/rtb4free_key.pem admin.html ubuntu@rtb4free.com:XRTB/web
