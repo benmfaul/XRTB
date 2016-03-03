539fb5d97941ad043061449462c611d89bab2552
Ben Faul <ben.faul@gmail.com>
2016-03-02 17:15:05 -0800

    Member message errors

--- src/com/xrtb/bidder/WebCampaign.java
+++ src/com/xrtb/bidder/WebCampaign.java
@@ -720,21 +720,16 @@ public class WebCampaign {
 				values.put("nobidreason", Configuration.getInstance().printNoBidReason);
 			} else {
 		  		Map info = Controller.getInstance().getMemberStatus(member);	
-				if (info != null) {
-					values.put("stopped",info.get("stopped"));
-					values.put("ncampaigns",info.get("ncampaigns"));
-					values.put("loglevel",info.get("loglevel"));
-					values.put("nobidreason", info.get("nobidreason")); 
-				} else
-					values = null;
-				
+				values.put("stopped",info.get("stopped"));
+				values.put("ncampaigns",info.get("ncampaigns"));
+				values.put("loglevel",info.get("loglevel"));
+				values.put("nobidreason", info.get("nobidreason")); 				
 				
 			} 
-			if (values == null) {
-				entry.put("name", member);
-				entry.put("values", values);
-				core.add(entry);
-			}
+			entry.put("name", member);
+			entry.put("values", values);
+			core.add(entry);
+
 		}
 		return core;
 	}
