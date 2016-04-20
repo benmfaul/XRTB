e27aaaad6d9a9b2daf9473309d7d7fd9a58c6f48
Ben Faul <ben.faul@gmail.com>
2016-04-20 12:43:24 -0700

    File cleanup

--- sitespecific.sh
+++
@@ -1,6 +0,0 @@
-mv Campaigns/payday.json Campaigns/payday.json.sav
-mv database.json database.json.sav
-rm libs/xrtb.jar
-mv web/admin.html web/login.html.sav
-mv web/exchange.html web/exchange.sav
-mv build.xml build.xml.sav
