c64984959e6e63d90097c23d7cb38825aa1a46fa
Ben Faul <ben.faul@gmail.com>
2016-05-16 14:08:00 -0700

    New curltest

--- shell/curltest.sh
+++ shell/curltest.sh
@@ -1,1 +1,1 @@
-curl -X POST -d @../SampleBids/bad.txt http://localhost:8080/rtb/bids/fyber --header "Content-Type:application/json"
+curl -X POST -d @../SampleBids/apptest.txt http://localhost:8080/rtb/bids/smaato --header "Content-Type:application/json"
