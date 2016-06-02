b2f5b9244d0a47b51f7f961a000ae1b098ed05cd
Ben Faul <ben.faul@gmail.com>
2016-06-01 18:00:57 -0700

    New blog post

--- shell/curltest.sh
+++ shell/curltest.sh
@@ -1,1 +1,1 @@
-curl -X POST -d @../SampleBids/apptest.txt http://localhost:8080/rtb/bids/smaato --header "Content-Type:application/json"
+curl -X POST -d @../SampleBids/nexage.txt http://localhost:8080/rtb/bids/smaato --header "Content-Type:application/json"
