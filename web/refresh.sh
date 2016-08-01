be0167ae6bd6f87b52151b15a5e178cf1ea3602b
Ben Faul <ben.faul@gmail.com>
2016-08-01 15:09:48 -0700

    Remove dependencies of thw website

--- web/refresh.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-scp -i ~ben/certs/rtb4free_key.pem exchange.html ubuntu@rtb4free.com:XRTB/web
-scp -i ~ben/certs/rtb4free_key.pem login.html ubuntu@rtb4free.com:XRTB/web
-scp -i ~ben/certs/rtb4free_key.pem admin.html ubuntu@rtb4free.com:XRTB/web
--- www/refresh.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-scp -i ~ben/certs/rtb4free_key.pem $1 ../html ubuntu@rtb4free.com:/usr/share/nginx/www
