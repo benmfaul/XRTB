9b06e877d52f83cdc4454357cb24ccbad53815e5
Ben Faul <ben.faul@gmail.com>
2016-10-22 12:34:09 -0700

    Crontabs

--- shell/mvit.sh
+++ shell/mvit.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+#
+# Moveit script
+#
+# Place in /etc/cron/hourly
+# Be sure to chmod +x
+#
 CERT=~ben/certs/rtb4free_key.pem
 RTB=ubuntu@rtb4free.com
 
@@ -5,8 +12,7 @@ WORKDIR=.
 
 DATE=$(date +%Y%m%d)
 TIME=$(date +%T)
-mkdir $WORKDIR/logs
-mkdir $WORKDIR/logs/$RTB
+mkdir -p $WORKDIR/logs/$RTB
 
 
 ssh -i $CERT $RTB sudo cp /var/log/rtb4free.log /var/log/rtb4free.log.$DATE.$TIME
@@ -14,7 +20,7 @@ ssh -i $CERT $RTB sudo truncate -s0 /var/log/rtb4free.log
 scp -i $CERT $RTB:/var/log/rtb4free.log.$DATE.$TIME $WORKDIR/logs/$RTB
 ssh -i $CERT $RTB sudo rm /var/log/rtb4free.log.$DATE.$TIME
 
-ssh -i $CERT $RTB sudo cp XRTB/logs/request request.$DATE.$TIME
+ssh -i $CERT $RTB sudo cp XRTB/logs/request XRTB/logs/request.$DATE.$TIME
 ssh -i $CERT $RTB sudo truncate -s0 XRTB/logs/request
 scp -i $CERT $RTB:XRTB/logs/request.$DATE.$TIME $WORKDIR/logs/$RTB
 
