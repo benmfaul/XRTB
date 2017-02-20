543cbf5ff381e783a7492429d7eb2908d27f8631
Ben Faul <ben.faul@gmail.com>
2017-02-20 11:14:50 -0800

    Add new exchange, adventurefeeds

---
+++ src/com/xrtb/exchanges/Adventurefeeds.java
@@ -0,0 +1,63 @@
+package com.xrtb.exchanges;
+
+import java.io.InputStream;
+
+import com.xrtb.pojo.BidRequest;
+
+/**
+ * A class to handle Adventurefeeds ad exchange
+ * @author Ben M. Faul
+ *
+ */
+
+public class Adventurefeeds extends BidRequest {
+
+        public Adventurefeeds() {
+                super();
+                parseSpecial();
+        }
+        
+        /**
+         * Make a Adventurefeeds bid request using a String.
+         * @param in String. The JSON bid request for Epom
+         * @throws Exception on JSON errors.
+         */
+        public Adventurefeeds(String  in) throws Exception  {
+                super(in);
+                parseSpecial();
+    }
+
+        /**
+         * Make a Adventurefeeds bid request using an input stream.
+         * @param in InputStream. The contents of a HTTP post.
+         * @throws Exception on JSON errors.
+         */
+        public Adventurefeeds(InputStream in) throws Exception {
+                super(in);
+                parseSpecial();
+        }
+        
+        /**
+         * Process special Epom stuff, sets the exchange name. Setss encoding.
+         */
+        @Override
+        public boolean parseSpecial() {
+                exchange = "adventurefeeds";
+                usesEncodedAdm = false;
+                return true;
+        }
+        
+    	/**
+    	 * Create a new Adventurefeeds object from this class instance.
+    	 * @throws JsonProcessingException on parse errors.
+    	 * @throws Exception on stream reading errors
+    	 */
+    	@Override
+    	public Adventurefeeds copy(InputStream in) throws Exception  {
+    		return new Adventurefeeds(in);
+    	}
+}
+
+
+
+        
