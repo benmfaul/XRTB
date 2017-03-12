6713a032f78231d0f10244b129c18ff828172906
Ben Faul <ben.faul.com>
2017-03-12 09:55:02 -0700

    Clean up source code removing unnecessary assigns.

--- src/com/xrtb/bidder/SSI.java
+++ src/com/xrtb/bidder/SSI.java
@@ -13,10 +13,10 @@ import java.nio.file.Paths;
 
 public class SSI {
 	
-	static final String PREAMBLE = "<!--#include virtual=\"";
+	private static final String PREAMBLE = "<!--#include virtual=\"";
 	public static String convert(String page) throws Exception {
 
-		if (page.indexOf(PREAMBLE) != -1) { // don't allocate resources when it is not needed.
+		if (page.contains(PREAMBLE)) { // don't allocate resources when it is not needed.
 			StringBuilder sb = new StringBuilder(page);
 			while(getTagValues(sb));
 
@@ -30,26 +30,18 @@ public class SSI {
 	 * Pluck the code between <% and %>, execute it, then take the result and replace the old text.
 	 * @param buf StringBuffer. The html page.
 	 * @return boolean. True if a tag was found, otherwise false.
-	 * @throws Exception on JavaScript language errors or unbalanced <% %> tags.
 	 */
-	private static boolean getTagValues(StringBuilder buf) throws Exception {
+	private static boolean getTagValues(StringBuilder buf) {
 		int start = buf.indexOf(PREAMBLE);
 		int stop = buf.indexOf("-->",start);
 		if (start < 0 || stop < 0)
 			return false;
 
 		String code = new String(buf.substring(start + PREAMBLE.length(), stop));
-		//Object o = engine.eval(code);
-		//if (o == null)
-		//	o = "";
-		//else if (o instanceof String)
-		//	o = o.toString();
-		//else
-		//	o = "";
 		
 		int tail = code.indexOf("\"");
 		code = code.substring(0, tail);
-		if (code.startsWith("www") == false) {
+		if (!code.startsWith("www")) {
 			code = "www/" + code;
 		}
 		
