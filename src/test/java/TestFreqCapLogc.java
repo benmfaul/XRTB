1c0c69081e5d76db9a78e8d78f38404647231522
Ben Faul <ben.faul@gmail.com>
2017-07-05 17:40:20 -0700

    Remove redundant test.

--- src/test/java/TestFreqCapLogc.java
+++ src/test/java/TestFreqCapLogc.java
@@ -29,32 +29,5 @@ public class TestFreqCapLogc {
 		assertNull(redisson.get("JUNK"));
 
 	}
-	
-	@Test
-	public void test1M() throws Exception {
-		AerospikeHandler client = AerospikeHandler.getInstance("localhost", 3000,300);
-		RedissonClient redisson = new RedissonClient(client);
-		Set<String> set = new HashSet();
-		
-		for (int i=0;i<1000; i++) {
-			String key = "JUNK" + i;
-			System.out.println(key);
-			set.add(key);
-			redisson.set(key,key,120);
-		}
-		
-		System.out.println("Assignment complete");
-		
-		for (String s : set) {
-			assertNotNull(redisson.get(s));
-		}
-		
-		System.out.println("Test 1 complete");
-		
-		Thread.sleep(125000);
-		for (String s : set) {
-			assertNull(redisson.get(s));
-		}
-		
-	}
+
 }
